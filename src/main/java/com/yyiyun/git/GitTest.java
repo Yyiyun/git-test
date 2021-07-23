cb11a017b3e782b8fc82591e197c537efdcde384
Eythavon <1446249443@qq.com>
2021-07-23 17:05:05 +0800

    second commit

--- src/main/java/com/yyiyun/git/GitTest.java
+++ src/main/java/com/yyiyun/git/GitTest.java
@@ -3,5 +3,6 @@ package com.yyiyun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
